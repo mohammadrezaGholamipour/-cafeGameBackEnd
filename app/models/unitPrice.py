ce75d928e64f2dfe8e9460f083881ab8b3e69d4e
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-02-02 15:53:56 +0330

    feat(api): set unique price and show error if not unique

--- app/models/unitPrice.py
+++ app/models/unitPrice.py
@@ -4,13 +4,22 @@ from sqlalchemy.orm import relationship
 from app.db.base import Base
 
 
+from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship
+
 class UnitPrice(Base):
-    __tablename__ = "unitPrice"  # نام جدول
+    __tablename__ = "unitPrice"
 
     id = Column(Integer, primary_key=True, index=True)
-    price = Column(Integer, nullable=False, index=True, unique=False)
+    price = Column(Integer, nullable=False, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
     owner = relationship("User", back_populates="unitPrice")
+
+    __table_args__ = (
+        UniqueConstraint("owner_id", "price", name="uq_owner_price"),
+    )
+
 
 
 from app.models.user import User
