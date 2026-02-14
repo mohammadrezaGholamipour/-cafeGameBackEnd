30deb6250fe241ab92d5ab7aa8aa6c32a88da31a
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-02-14 21:19:24 +0330

    feat(api): add endpoint to get authenticated user's bills

--- app/api/v1/bill.py
+++ app/api/v1/bill.py
@@ -1,8 +1,9 @@
-from app.models.console import Console
-from app.models.unitPrice import UnitPrice
 from app.schemas.bill import BillCreate, BillWithOwner, BillWithOutDetails
 from app.core.security import get_current_user
+from app.models.unitPrice import UnitPrice
+from fastapi import HTTPException, status
 from fastapi import APIRouter, Depends
+from app.models.console import Console
 from sqlalchemy.orm import Session
 from datetime import datetime, UTC
 from app.db.session import get_db
@@ -16,7 +17,7 @@ router = APIRouter(
     dependencies=[Depends(get_current_user)]
 )
 
-from fastapi import HTTPException, status
+
 
 
 @router.post(
@@ -29,7 +30,6 @@ def create_bill(
         current_user: Annotated[User, Depends(get_current_user)],
         db: Session = Depends(get_db),
 ):
-    # 1️⃣ چک کن کنسول وجود داشته باشد
     console = (
         db.query(Console)
         .filter(Console.id == bill_data.console_id)
@@ -95,6 +95,7 @@ def create_bill(
     return new_bill
 
 
+
 @router.get(
     "/list",
     response_model=list[BillWithOwner]
@@ -109,80 +110,20 @@ def list_all_bills(
     )
     return bills
 
-# @router.get(
-#     "/my-bills",
-#     response_model=list[BillOut]
-# )
-# def list_my_bills(
-#         current_user: Annotated[User, Depends(get_current_user)],
-#         db: Session = Depends(get_db),
-# ):
-#     bills = (
-#         db.query(Bill)
-#         .filter(Bill.owner_id == current_user.id)
-#         .all()
-#     )
-#     return bills
-#
-#
-# @router.patch(
-#     "/{bill_id}",
-#     response_model=BillOut
-# )
-# def update_bill(
-#         bill_id: Annotated[int, Path(..., gt=0)],
-#         bill_data: BillUpdate,
-#         current_user: Annotated[User, Depends(get_current_user)],
-#         db: Session = Depends(get_db),
-# ):
-#     bill = db.query(Bill).filter(Bill.id == bill_id).first()
-#
-#     if not bill:
-#         raise HTTPException(status_code=404, detail="فاکتور یافت نشد")
-#
-#     if bill.owner_id != current_user.id:
-#         raise HTTPException(status_code=403, detail="اجازه دسترسی ندارید")
-#
-#     for key, value in bill_data.dict(exclude_unset=True).items():
-#         setattr(bill, key, value)
-#
-#     db.commit()
-#     db.refresh(bill)
-#
-#     return bill
-#
-#
-# @router.delete(
-#     "/{bill_id}",
-#     status_code=status.HTTP_204_NO_CONTENT
-# )
-# def delete_bill(
-#         bill_id: Annotated[int, Path(..., gt=0)],
-#         current_user: Annotated[User, Depends(get_current_user)],
-#         db: Session = Depends(get_db),
-# ):
-#     bill = db.query(Bill).filter(Bill.id == bill_id).first()
-#
-#     if not bill:
-#         raise HTTPException(status_code=404, detail="فاکتور یافت نشد")
-#
-#     if bill.owner_id != current_user.id:
-#         raise HTTPException(status_code=403, detail="اجازه دسترسی ندارید")
-#
-#     db.delete(bill)
-#     db.commit()
-#
-#
-# @router.get(
-#     "/list",
-#     response_model=list[BillOut]
-# )
-# def list_all_bills(
-#         db: Session = Depends(get_db),
-# ):
-#     bills = (
-#         db.query(Bill)
-#         .order_by(Bill.id.desc())
-#         .all()
-#     )
-#     return bills
+
+
+@router.get(
+    "/my-bills",
+    response_model=list[BillWithOutDetails]
+)
+def list_my_bills(
+        current_user: Annotated[User, Depends(get_current_user)],
+        db: Session = Depends(get_db),
+):
+    bills = (
+        db.query(Bill)
+        .filter(Bill.owner_id == current_user.id)
+        .all()
+    )
+    return bills
+
