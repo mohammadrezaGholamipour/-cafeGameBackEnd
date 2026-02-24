f2c673c47d97c2be922892cdd0d7ea7c21521276
mohammadrezaGholamipour <mrzgp2016@gmail.com>
2026-02-24 15:35:16 +0330

    fix(update) change port

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ app = FastAPI(title="CafeGame", description="API for managing CafeGame", version
 
 import os
 port = int(os.environ.get("PORT", 8080))
-uvicorn.run(app, host="0.0.0.0", port=port)
+uvicorn.run(app, host="127.0.0.1", port=port)
 
 download_db()
 
